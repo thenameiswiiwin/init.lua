c287e33a2a342cdb7f5aae29dd649376d4c6087b
thenameiswiiwin <huyn.nguyen95@gmail.com>
2025-07-11 20:46:39 -0400

    disabled: snack.dashboard, snack.notifier

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  -- disable copilot
+  -- { "zbirenbaum/copilot.lua", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  { "folke/snacks.nvim", opts = { dashboard = { enabled = false }, notifier = { enabled = false } } },
+}
